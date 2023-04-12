cb3cf61288fcc8a9bc1cdbe3c94071f92039793d
Brejcha13320 <davidmejia-2000@outlook.com>
2023-04-12 08:22:09 -0500

    first commit

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private router: Router, private authService: AuthService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | boolean {
+    return this.authService.verificaAutenticacion().pipe(
+      tap((estaAutenticado) => {
+        if (!estaAutenticado) {
+          this.router.navigate(['/auth/login']);
+        }
+      })
+    );
+  }
+
+  canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | boolean {
+    return this.authService.verificaAutenticacion().pipe(
+      tap((estaAutenticado) => {
+        if (!estaAutenticado) {
+          this.router.navigate(['/auth/login']);
+        }
+      })
+    );
+  }
+}
